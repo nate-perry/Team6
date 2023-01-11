f9e2af530191ae4fecf627de813812a52531e306
arshiv27 <47725534+arshiv27@users.noreply.github.com>
2023-01-10 23:43:22 -0500

    pacman-is_ghost_in_range (#27)

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -74,9 +74,21 @@ public class PacMan {
   }
 
   public boolean is_ghost_in_range() {
-    return false;
-  }
+    ArrayList<Location> places = new ArrayList<Location>();
+    
+    places.add(new Location(this.myLoc.x + 1, this.myLoc.y + 0)); 
+    places.add(new Location(this.myLoc.x - 1, this.myLoc.y - 0));
+    places.add(new Location(this.myLoc.x + 0, this.myLoc.y + 1));
+    places.add(new Location(this.myLoc.x - 0, this.myLoc.y - 1));
 
+    for (Location location:places) {
+       HashSet<Map.Type> checker = myMap.getLoc(location);
+       if (checker.contains(Map.Type.GHOST)) {
+	return true;
+       }
+    }
+   return false; 
+  }
   public JComponent consume() {
     return null;
   }
